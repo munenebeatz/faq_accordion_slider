9d77773e44a54784dc37abfc0aa890125d97f922
Munenebeatz <md@cordelia.co.ke>
2016-09-05 14:34:13 +0300

    transform nav arrow / toggleClass

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
 /**
+ * 
  * Created by CORDELIA on 05-Sep-16.
  */
+
+(function ($) {
+    var action = "click",
+        speed = 300;
+
+    $('.q').on(action, function (e) {
+        //get next element and slideToggle other answers
+        $(this).next().slideToggle(speed).siblings('li.a').slideUp(speed);
+        //get active nav arrow
+        var img = $(this).children('img');
+        //remove rotate class from all other img except the active
+        $('img').not(img).removeClass('rotate');
+        //toggle rotate class
+        img.toggleClass('rotate');
+    });
+})(jQuery);
